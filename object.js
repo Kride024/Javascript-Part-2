bf4f1df6a37fbde3be636ef6e3d146d94bcb3c5f
Kriti yadav <156906180+Kride024@users.noreply.github.com>
2025-01-23 22:07:26 +0530

    commit

---
+++ object.js
@@ -0,0 +1,10 @@
+//! Javascript Object
+
+//* What is an Object?
+
+//? Object are fundamental part of javascript providing a way to group related data and function together. In javascript, an object is a collection of key-value pairs,where each key is string (or a symbol)and each value can be any data type,including other object.
+// object can have properties and method,making them versatile for various use cases. 
+// grp together data type string number,method, function even objects
+
+//syntax- obj={}
+//arr=[],obj={},str="",char=''
