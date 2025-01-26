375795d19caf6f8817c93cab9d33f12581568bea
Kriti yadav <156906180+Kride024@users.noreply.github.com>
2025-01-27 01:19:50 +0530

    Commit

--- Ecmascripts/ES-2020.js
+++ Ecmascripts/ES-2020.js
@@ -5,7 +5,7 @@
 //? List of new Useful features added in ES8 👇
 //BigInt
 //Nullish Coalescing Operator ??
-//Optional Chaining Operator ?
+//Optional Chaining Operator ?.
 //promise.allSettled
 
 //*---------------------------------------
@@ -34,7 +34,51 @@ console.log(maxNumber);
 let favNum=0;//falsy
 //userFavNum=favNum||10; //? Earlier Problem
 //userFavNum=favNum??10;//? Modern solution
-console.log(userFavNum);
+//console.log(userFavNum);
+
+//*---------------------------------------
+//* Optional Chaining Operator(?.)
+//*---------------------------------------
+
+//? It provides a concise way to access properties of an object without worrying about the existence of intermediate properties.It's particularly useful when working with nested objects or accessing properties of objects that may be null or undefined.
 
 
+const person={
+    name:"John",
+    address:{
+        city:"New York",
+        zipCode:12345,
+    },
+};
 
+//?Accessing nested properties without optional chaining
+//const city=person.address;
+//* Example-1:
+// const city=person.address?person.address.city:"city is not present";
+// console.log(city);
+//This method doesn't do error handling
+
+//?with optional chaining
+const city=person?.address?.city??"not found";
+console.log(city);
+
+//* Example-2:
+const Route={
+name:"John",
+address:{
+    city:"New York",
+    zipCode:12345,
+    coordinates:{
+        latitude:40.7128,
+        longitude:-74.006,
+    },
+   }, 
+  };
+
+const latitude=Route?.address?.coordinates?.latitude??"not found bhai";
+console.log(latitude);
+
+//*---------------------------------------
+//* Promise.allSettled()
+//*---------------------------------------
+//? We will cover later in our promises section part of the video and you gonna love that part.
