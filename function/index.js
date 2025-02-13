a491d53d579a3b0000c115922f710fc7e209aae2
Kriti yadav <156906180+Kride024@users.noreply.github.com>
2025-02-13 20:11:28 +0530

    Commit

--- function/index.js
+++ function/index.js
@@ -82,3 +82,36 @@ console.log(mathOperation(5,15,add));
 console.log(mathOperation(5,15,sub));
 
 
+ //*____________________________________________
+ //*   Callback hell
+ //*___________________________________________
+
+ //? Callback hell, also known as the Pyramid of Doom, refers to a situation in asynchronous JavaScript programming where multiple nested callbacks ara used to handle asynchronous operations.This often results in code that is difficult to read,understand,and maintain due to its deeply nested structure.
+
+ const getStudentData=()=>{
+    setTimeout(()=>{
+        console.log("Hi,My name is vinod");
+        setTimeout(()=>{
+            console.log("kya bhai");
+            setTimeout(()=>{
+                console.log("knight");
+                setTimeout(()=>{
+                    console.log("hum");
+                    setTimeout(()=>{
+                        console.log("krishna");
+                        
+                    },1000);                    
+                },1000);
+                
+            },1000);
+            
+        },1000);
+        
+    },1000);
+ };
+ getStudentData();
+
+  //*____________________________________________
+ //*   Promises:
+ //*___________________________________________
+//? A promise is like a placeholder for the result of an asynchronous operation.
